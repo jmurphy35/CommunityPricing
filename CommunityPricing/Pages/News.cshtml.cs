ce8b3f0a0c1a06e10b26d81e07a80ee5875f9165
John <jmurphy35@gmail.com>
2020-01-30 20:11:06 -0500

    Made a news page

---
+++ CommunityPricing/Pages/News.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CommunityPricing.Pages.Shared
+{
+    [AllowAnonymous]
+    public class NewsModel : PageModel
+    {
+       
+        public void OnGet()
+        {
+
+        }
+    }
+}
